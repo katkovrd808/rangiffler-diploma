f5475ffca038af7a7717f6e8ff2fb5ad4489964f
Roman Katkov <romangadle@gmail.com>
2025-11-25 20:32:59 +0700

    Rangiffler-photos - friendship create date fix

--- rangiffler-userdata/src/main/resources/db/migration/rangiffler-userdata/V1__schema_init.sql
+++ rangiffler-userdata/src/main/resources/db/migration/rangiffler-userdata/V1__schema_init.sql
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS "user"
 
 CREATE TABLE IF NOT EXISTS "friendship"
 (
-    requester_id UUID    NOT NULL,
-    addressee_id UUID    NOT NULL,
-    created_date date      NOT NULL,
+    requester_id UUID          NOT NULL,
+    addressee_id UUID          NOT NULL,
+    created_date date          NOT NULL DEFAULT CURRENT_DATE,
     status varchar(50)         NOT NULL,
     PRIMARY KEY (requester_id, addressee_id),
     CONSTRAINT friend_are_distinct_ck CHECK (requester_id <> addressee_id),
